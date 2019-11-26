af48f23296b34a468d27760fe6fdcdf698e7786f
olaviolacerda <olavio.lacerda@hotmail.com>
2019-11-26 01:27:58 -0300

    added button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+export default function Button({className, children, handleClick}) {
+  return (
+    <button className={className} type="button" onClick={handleClick}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  handleClick: PropTypes.func,
+  children: PropTypes.node,
+};
+
+Button.defaultProps = {
+  className: 'button',
+  children: 'See more',
+  handleClick: () => console.log('clicked'),
+};
