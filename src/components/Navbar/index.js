8ebcaa4ab2a46db0229b0242b608dce9afefaa19
olaviolacerda <olavio.lacerda@hotmail.com>
2019-11-26 01:26:55 -0300

    :construction: added components for navbar and footer

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+import logo from '../../assets/images/logo.png';
+
+export default function Navbar() {
+  return (
+    <navbar>
+      <img src={logo} alt="Star Wars Logo" />
+    </navbar>
+  );
+}
