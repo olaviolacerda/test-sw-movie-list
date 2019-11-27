0abfa0f5ce9b30d81cebb6c1fe871647424a7724
olaviolacerda <olavio.lacerda@hotmail.com>
2019-11-27 00:51:10 -0300

    added gsap library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './styles/index.scss';
 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import Routes from './routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <div className="container">
+    <Navbar />
+    <Routes />
+    <Footer />
+  </div>
+);
 
 export default App;
