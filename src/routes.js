00fecbe50cdace746bea102f8f33666920dd39a4
olaviolacerda <olavio.lacerda@hotmail.com>
2019-11-27 00:51:56 -0300

    added route to movie details

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Showcase from './screens/Showcase';
+import MovieDetails from './screens/MovieDetails';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Showcase} />
+      <Route path="/movies/:movieId/details" component={MovieDetails} />
     </BrowserRouter>
   );
 }
