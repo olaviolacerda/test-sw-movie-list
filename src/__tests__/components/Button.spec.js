a2e3f831b060d8d93d3cf56ad0ca51536e6e6053
olavio.lacerda <olavio.lacerda@4all.com>
2019-12-10 11:10:54 -0300

    added some unit tests

--- src/__tests__/components/Button.spec.js
+++ src/__tests__/components/Button.spec.js
@@ -5,5 +5,26 @@ import Button from '../../components/Button';
 describe('Component Button test', () => {
   it('should render without crashing', () => {
     shallow(<Button />);
+
+    expect(Button).toBeDefined();
+  });
+
+  it('should render correctly', () => {
+    const buttonElement = shallow(
+      <Button className="button" onClick={() => {}} type="button">
+        button test
+      </Button>
+    );
+
+    expect(buttonElement).toMatchSnapshot();
+  });
+
+  it('should have a button value', () => {
+    const buttonElement = shallow(
+      <Button className="button" onClick={() => {}} type="button">
+        button test
+      </Button>
+    );
+    expect(buttonElement.props().children).toEqual('button test');
   });
 });
